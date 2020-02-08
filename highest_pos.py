016143295376859addc38327622d540ef6f42824
fandassadyk <46761654+fandassadyk@users.noreply.github.com>
2020-02-08 13:01:23 +0300

    Add files via upload

--- highest_pos.py
+++ highest_pos.py
@@ -1,6 +1,6 @@
-import pyodbc
+import pyodbc, re
 import pandas as pd
-from itertools import chain
+
 
 
 class Position:
@@ -8,6 +8,15 @@ class Position:
         self.DB_path = DB_path
         self.connStr = pyodbc.connect(r'DRIVER={Microsoft Access Driver (*.mdb)};' + r'DBQ={};'.format(self.DB_path) + r'PWD=xamok')
         self.cursor = self.connStr.cursor()
+
+
+    def do_commit(self):
+        self.connStr.commit()
+
+
+    def close_connection(self):
+        self.cursor.close()
+        self.connStr.close()
 
 
     def current_wire_pos(self):
@@ -18,8 +27,6 @@ class Position:
         :param DB_path: string, path to the database on the computer
         :return dict: dict, contains values of the highest position from table JOBS
         """
-
-        #cursor.execute("SELECT TOP 1 ArticleID from articles")
         self.cursor.execute("SELECT TOP 1 ArticleID from jobs")
         try:
             ArticleID = self.cursor.fetchall()[0][0]
@@ -39,17 +46,12 @@ class Position:
         :return: dict: dict, contains values of 1 row
         '''
 
-        # columns = ['amount', 'harness', 'marking', 'wire_number', 'wire_square', 'wire_color', 'wire_length',
-        #           'wire_seal_1', 'wire_cut_length_1', 'wire_terminal_1',
-        #           'wire_seal_2', 'wire_cut_length_2', 'wire_terminal_2'] #name of columns from wire chart
-        columns = ['amount', 'harness', 'wire_square', 'wire_color', 'wire_length',
+        columns = ['amount', 'harness', 'wire_number', 'wire_square', 'wire_color', 'wire_length',
                    'wire_seal_1', 'wire_cut_length_1', 'wire_terminal_1',
-                   'wire_seal_2', 'wire_cut_length_2', 'wire_terminal_2']
+                   'wire_seal_2', 'wire_cut_length_2', 'wire_terminal_2'] #name of columns from wire chart
         dict = {}
         #ArticleID is OK
         if ArticleID is not None:
-            # cursor.execute("SELECT [(1-2) WireID], [(1-2) WireLength], [(1) StrippingLength], [(1) SealId], [(1) TerminalID],"
-            #               "[(2) StrippingLength], [(2) SealId], [(2) TerminalID] from leadsets")
             self.cursor.execute("SELECT TotalPieces FROM jobs WHERE ArticleID={}".format(ArticleID))
             amount = [self.cursor.fetchall()[0][0]]
             # amount = 64
@@ -57,17 +59,17 @@ class Position:
                                 "WHERE ArticleGroupID=(SELECT ArticleGroupID FROM articles WHERE ArticleID={})".format(
                 ArticleID))
             harness_name = [self.cursor.fetchall()[0][0]]
+            wire_number = self.wire_number(ArticleID)
             square, color = self.square_and_color(ArticleID)
             self.cursor.execute("SELECT [(1-2) WireLength], [(1) SealID], [(1) StrippingLength], [(1) TerminalID], "
                                 "[(2) SealID], [(2) StrippingLength], [(2) TerminalID] FROM leadsets "
                                 "WHERE ArticleID={}".format(ArticleID))
-            row = amount + harness_name + [square] + [color] + list(self.cursor.fetchall()[0])
+            row = amount + harness_name + [wire_number] + [square] + [color] + list(self.cursor.fetchall()[0])
             for i in range(len(columns)):
                 dict[columns[i]] = row[i]
         else:
             for i in range(len(columns)):
                 dict[columns[i]] = 0
-
         return dict
 
 
@@ -95,22 +97,44 @@ class Position:
         return float(wire_key[0]), color
 
 
+    def wire_number(self, ArticleID):
+        '''
+        Func finds wire_number in leadsets table from Komax database
+        :param ArticleID: int
+        :return: wire_number: string or None
+        '''
+        self.cursor.execute("SELECT [(1-2) InkjetActions] FROM leadsets WHERE ArticleID={}".format(ArticleID))
+        str = self.cursor.fetchall()[0][0]
+        if str != 'None':
+            m = re.search('<TextNormal>(.*?)<\/TextNormal>', str)
+            return m.group(1)
+        else:
+            return None #if wire does not have his own wire_number
+
+
     def stop_komax(self, status):
         '''
         Func uses for stopping work Komax. Deletes all positions from JOBS
-        :param status: string,
-        :return: df: dataframe with all positions except the first that were in table JOBS; or None
+        :param status: string
+        :return: df: dataframe with all positions except the first that were in table JOBS; or None; or string: 'deleted'
         '''
 
-        # TODO:
-        #  add try-except for checking SELECT TOP 1 ArticleID from jobs
+        self.cursor.execute("SELECT TOP 1 ArticleID from jobs")
+        try:
+            ArticleID = self.cursor.fetchall()[0][0]
+        except:
+            ArticleID = None
 
-        df = self.create_dataframe()
-        #self.cursor.execute("DELETE FROM jobs WHERE ArticleID<>(SELECT TOP 1 ArticleID from jobs)")
-        self.cursor.execute("DELETE FROM jobs WHERE ArticleID<>(SELECT TOP 1 ArticleID from jobs)")
-        if status == 'delete and save':
-            return df
-        elif status == 'delete': #if we do not need in rows from JOBS
+
+        if ArticleID is not None:
+            df = self.create_dataframe()
+            self.cursor.execute("DELETE FROM jobs WHERE JobPos<>(SELECT TOP 1 JobPos from jobs)")
+            self.do_commit()
+            if status == 'delete and save':
+                return df
+            elif status == 'delete': #if we do not need in records from JOBS
+                return 'deleted'
+        else:
             return None
 
 
@@ -137,37 +161,17 @@ class Position:
         return df
 
 
-    def delete_row(self):
-        self.cursor.execute("DELETE DISTINCTROW FROM seals")
-
-
-    def test(self):
-        d1 = {'amount': 80, 'harness': '451041105099-90', 'wire_square': 1.5, 'wire_color': 'КчЖ', 'wire_length': 410,
-         'wire_seal_1': 0, 'wire_cut_length_1': 16.0, 'wire_terminal_1': 0, 'wire_seal_2': 0, 'wire_cut_length_2': 3.8,
-         'wire_terminal_2': 150}
-        d2 = {'amount': 56, 'harness': '451041105099-78', 'wire_square': 1.0, 'wire_color': 'ЖГ', 'wire_length': 350, 'wire_seal_1': 0, 'wire_cut_length_1': 14.0, 'wire_terminal_1': 0, 'wire_seal_2': 0, 'wire_cut_length_2': 6.7, 'wire_terminal_2': 190}
-        d3 = {'amount': 23, 'harness': '451041105099-84', 'wire_square': 2.0, 'wire_color': 'ЖГ', 'wire_length': 350, 'wire_seal_1': 0, 'wire_cut_length_1': 14.0, 'wire_terminal_1': 0, 'wire_seal_2': 0, 'wire_cut_length_2': 3.7, 'wire_terminal_2': 200}
-
-        ds = [d1, d2, d3]
-        d = {}
-        for k in d1.keys():
-            d[k] = [d[k] for d in ds]
-        #print(d)
-        df = pd.DataFrame(d)
-        #print(df)
-
 
 
 
 #DB_path = 'C:\Komax\Data\TopWin\DatabaseServer.mdb'
-#DB_path = 'C:\Komax\Data\TopWin\DatabaseServerXP.mdb'
-DB_path = 'C:\Komax\Data\TopWin\DatabaseServer(1).mdb'
+DB_path = 'C:\Komax\Data\TopWin\DatabaseServerXP.mdb'
+#DB_path = 'C:\Komax\Data\TopWin\DatabaseServer(1).mdb'
 
 
 x = Position(DB_path)
 #dict = x.current_wire_pos()
 #print(dict)
-#result = x.stop_komax('delete and save')
-#print(result)
-#x.test()
-x.delete_row()
+
+result = x.stop_komax('delete and save')
+print(result)
